728121b66011d163655691a25e8b62872bc28529
Sergio Alves Barbosa <sergio.alvesbarbosa@gmail.com>
2024-03-26 18:48:39 -0300

    start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RequestLoggingMiddleware } from './logger/request-logging.middleware';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv'; // Importe o pacote dotenv
+
+// Carregue as variáveis de ambiente do arquivo .env
+dotenv.config();
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://sergioalvesbarbosa:a2fKhfZPpf1wCPz7@cluster0.yws5smc.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(process.env.DB_URL), // Use a variável de ambiente para a URL do banco de dados
     AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
-
-//Senha banco: a2fKhfZPpf1wCPz7
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggingMiddleware).forRoutes('*');
+  }
+}
---
+++ src/logger/request-logging.middleware.ts
@@ -0,0 +1,23 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class RequestLoggingMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl, ip } = req;
+    const userAgent = req.get('user-agent') || '';
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const contentLength = res.get('content-length');
+
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${ip} - ${userAgent}`,
+      );
+    });
+
+    next();
+  }
+}
