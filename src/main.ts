840ba9b7ab4fad716e05b46b7e97baed6d5c9fb3
Sergio Alves Barbosa <sergio.alvesbarbosa@gmail.com>
2024-04-01 14:06:41 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  // Configuração do CORS
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
